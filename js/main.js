0bcc983a03f94e29466ac0ce42650eb3a7caef3c
Steven Gliebe <3372396+stevengliebe@users.noreply.github.com>
2020-05-18 08:57:16 -0500

    👌 IMPROVE: Focus on textarea after made visible

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
 
     // Show textarea when click "copy and paste"
     $('#wie-content-toggle').on('click', function () {
-        $('#wie-import-data').fadeIn('fast');
+        $('#wie-import-data').fadeIn('fast').focus();
     });
 
 });
